c7ef4a29a15cd08a31ee58bd9bfc7cf8d04ee0dc
Realish <ra17g@fsu.edu>
2024-04-09 15:05:16 -0400

    dataset choices update in main

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_argparser():
     parser.add_argument("--data_root", type=str, default='./datasets/data',
                         help="path to Dataset")
     parser.add_argument("--dataset", type=str, default='voc',
-                        choices=['voc', 'cityscapes'], help='Name of dataset')
+                        choices=['voc', 'cityscapes', 'coco'], help='Name of dataset')
     parser.add_argument("--num_classes", type=int, default=None,
                         help="num classes (default: None)")
 
